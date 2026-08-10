70ac07ff022f269f226845a4decddb28f5db872e
Jinwon <jinone61@gmail.com>
2026-08-10 20:24:21 +0900

    Update api url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,8 +1,8 @@
 import { defineBoot } from '#q-app'
 import axios from 'axios'
 
-// const api = axios.create({ baseURL: 'http://3.38.133.139:8080' })
-const api = axios.create({ baseURL: 'http://localhost:8080' })
+const api = axios.create({ baseURL: 'http://3.38.133.139:8080' })
+// const api = axios.create({ baseURL: 'http://localhost:8080' })
 
 export default defineBoot(({ app }) => {
   app.config.globalProperties.$axios = axios
